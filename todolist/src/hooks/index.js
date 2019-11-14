ced6b3c2856ef5249fd2c245814e71dc21e38121
Cristian371 <czend371@gmail.com>
2019-11-13 19:37:47 -0600

    created scss file

--- todolist/src/hooks/index.js
+++ todolist/src/hooks/index.js
@@ -11,7 +11,7 @@ export const useTasks = selectedProject => {
         let unsubscribe = firebase
             .firestore()
             .collection("tasks")
-            .where("userId", "==", "wDhaVmm9c5RRwIFdi8zx");
+            .where("userId", "==", "11");
 
         unsubscribe = selectedProject && !collatedTasksExist(selectedProject) ?
             (unsubscribe = unsubscribe.where("projectId", "==", selectedProject))
@@ -51,7 +51,7 @@ export const useProjects = () => {
         firebase
             .firestore()
             .collection("projects")
-            .where("userId", "==", "mCXZGeQBgIGtPl9LtGHp")
+            .where("userId", "==", "11")
             .orderBy("projectId")
             .get()
             .then(snapshot => {
