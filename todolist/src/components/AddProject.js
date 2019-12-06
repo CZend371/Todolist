2881744b8b81c95773016ccc4dcf0d58fd16351e
Cristian371 <czend371@gmail.com>
2019-12-05 18:02:22 -0600

    building add project component

--- todolist/src/components/AddProject.js
+++ todolist/src/components/AddProject.js
@@ -1,3 +1,48 @@
 import React, { useState } from "react";
+import { firebase } from "../firebase";
+import { generatePushId } from "../helpers";
+import { useProjectsValue } from "../context";
 
-export const AddProject = () => <p>Hello Add Project!</p>;
+
+export const AddProject = ({ shouldShow = false }) => {
+    const [show, setShow] = useState(shouldShow);
+    const [projectName, setProjectName] = useState('');
+
+    const projectId = generatePushId();
+    const { setProjects } = useProjectsValue();
+
+
+    const addProject = () =>
+        projectName &&
+        firebase
+            .firestore()
+            .collection('projects')
+            .add({
+                projectId,
+                name: projectName,
+                userId: 'mCXZGeQBgIGtPl9LtGHp'
+            })
+            .then(() => {
+                setProjects([]);
+                setProjectName('');
+                setShow(false);
+            });
+
+
+    return (
+        <div className='add-project' data-testid="add-project">
+            {show && (
+                <div className="add-project__input">
+                    <input
+                        value={projectName}
+                        onChange={e => setProjectName(e.target.value)}
+                        className="add-project__name"
+                        data-testid="project-name"
+                        type="text"
+                        placeholder="Name your project"
+                    />
+                </div>
+            )}
+        </div>
+    )
+};
