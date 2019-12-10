4e8cc192f0226c582c3938d39b41141e997dd670
Cristian371 <czend371@gmail.com>
2019-12-09 20:04:05 -0600

    added parameters for dark mode in header

--- todolist/src/components/TaskDate.js
+++ todolist/src/components/TaskDate.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { FaSpaceShuttle } from "react-icons/fa";
+import { FaSpaceShuttle, FaSun, FaRegPaperPlane } from "react-icons/fa";
 import moment from "moment";
 
 
@@ -14,10 +14,34 @@ export const TaskDate = ({ setTaskDate, showTaskDate, setShowTaskDate }) => show
                 data-testid="task-date-overlay"
             >
                 <span>
-
+                    <FaSpaceShuttle />
                 </span>
+                <span>Today</span>
             </li>
-
+            <li
+                onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate(moment().add(1, 'day').format('DD/MM/YYYY'));
+                }}
+                data-testid="task-date-tomorrow"
+            >
+                <span>
+                    <FaSun />
+                </span>
+                <span>Tomorrow</span>
+            </li>
+            <li
+                onClick={() => {
+                    setShowTaskDate(false);
+                    setTaskDate(moment().add(7, 'days').format('DD/MM/YYYY'));
+                }}
+                data-testid="task-date-next-week"
+            >
+                <span>
+                    <FaRegPaperPlane />
+                </span>
+                <span>Next Week</span>
+            </li>
         </ul>
     </div>
 );
